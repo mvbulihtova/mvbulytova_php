402e01ec4a661bfb29cf1acf475ff7cca7713876
mvbulytova <bulytova.m@yandex.ru>
2025-02-22 00:30:37 +0300

    hm1

---
+++ hw1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HW1Bulytova</title>
+</head>
+
+<body>
+    <header>
+        <img src="./img/polytech_logo_main_RGB.jpeg" alt="logo" class="logo"
+        style="height: 50px;">
+        <h1 style="color: dimgray;">Домашнее задание №1</h1>
+    </header>
+    <main>
+        <P>
+            <?php echo 'Have a good day!';
+            ?>
+        </P>
+    </main>
+    <footer>
+        <p style="color: dimgray;">Выполнила: Булытова Мария</p>
+    </footer>
+</body>
+
+</html>
